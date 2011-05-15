05a1ba246aa16b5a3bc4233af2a81d2628645745
Espen Stene Brønstad <espensb@stud.ntnu.no>
2011-05-15 15:10:14 +0200

    Start plotting

--- doplot.sh
+++ doplot.sh
@@ -10,5 +10,5 @@ $gcc isingspinn.c -o isingspinn.o
 time=`date +%s`
 ./isingspinn.o a > data.dat
 let time=`date +%s`-$time
-echo "Done in $time sec\n" 
+echo "Done in $time sec" 
 gnuplot doplot.gnu
